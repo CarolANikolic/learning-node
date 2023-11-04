fcfb2717422686d1f053fe185f5def3cfe7f1020
Carol A. Nikolic <carolnikolic@gmail.com>
2023-11-04 16:02:48 +0100

    feat: use appendFile to add a second sentence to the message.txt file.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const { writeFile } = require("fs");
+const { writeFile, appendFile} = require("fs");
 
-writeFile("message.txt", "Hello from NodeJS", (err) => {
+
+writeFile("message.txt", "Hello from NodeJS!", (err) => {
     if (err) throw err;
-    console.log("The file has been saved!")
+    console.log("The file has been saved.")
+});
+
+appendFile("./message.txt", "\nHello from Carol!", (err) => {
+    if (err) throw err;
+    console.log("The second sentence is appended.")
 });
