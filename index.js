9c42a1a3ed35f4a7ceeb11df10267ab06b6f253b
Carol A. Nikolic <carolnikolic@gmail.com>
2023-11-06 10:54:54 +0100

    feat: learn about node packages with console log sillyName.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { writeFile, appendFile, readFile} from "fs";
+import generateName from "sillyname";
 
-
+// Node native modules
 writeFile("message.txt", "Hello from NodeJS!", (err) => {
     if (err) throw err;
     console.log("The file has been saved.")
@@ -15,3 +16,8 @@ readFile("./message.txt", "utf8", (err, data) => {
     if (err) throw err;
     console.log(data)
 });
+
+// NPM packages
+let sillyName = generateName();
+console.log(`My name is ${sillyName}.`)
+
